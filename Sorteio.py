822015b587fa37cc580b76456cd88432355b447c
David Júnior <146877886+GitDavidJr@users.noreply.github.com>
2024-08-06 17:09:15 -0300

    Mult sorteios commit

--- Sorteio.py
+++ Sorteio.py
@@ -1,31 +1,24 @@
 import flet as ft
 
-numero: str
+lista = []  
 tamahoCaixa = 200
+indice = 0
+first = True
 
 
 def main(page: ft.Page):
 
-    numeroSorteado = ft.TextField(label="Numero que vai ser sorteado", width=tamahoCaixa)
+    numeroSorteado = ft.TextField(label="Numeros que vão ser sorteados \n(Coloque tudo junto separado apenas com virgula)", width=500)
 
     sorteio_img = ft.Image(src="sorteio.png", width=600, height=140)
 
     numeroInicio = ft.TextField(label="Primeiro numero", width=tamahoCaixa)
     numeroFim = ft.TextField(label="Ultimo numero", width=tamahoCaixa)
 
-    def sortearNum(e):
-        page.add(ft.Text(
-        f"NUMERO SORTEADO: {numero}",
-        size=40,
-        color=ft.colors.BLUE,
-        weight=ft.FontWeight.BOLD,
-        italic=True
-    ))
-
     def start(e):
 
-        global numero
-        numero = numeroSorteado.value
+        global lista
+        lista = numeroSorteado.value.split(",")
         page.clean()
         page.add(
             ft.Column(
@@ -39,6 +32,27 @@ def main(page: ft.Page):
             horizontal_alignment=ft.CrossAxisAlignment.CENTER,
             )   
         )
+
+    text_sorteado = ft.Text(
+            size=40,
+            color=ft.colors.BLUE,
+            weight=ft.FontWeight.BOLD,
+            italic=True
+            )
+
+    def sortearNum(e):
+        global indice
+        global first
+
+        if first:
+            text_sorteado.value = f"NUMERO SORTEADO: {lista[indice]}"
+            page.add(text_sorteado)
+            first = False
+        else:
+            text_sorteado.value = f"NUMERO SORTEADO: {lista[indice]}"
+            page.update()
+
+        indice += 1
 
     page.vertical_alignment=ft.MainAxisAlignment.CENTER
     page.horizontal_alignment=ft.CrossAxisAlignment.CENTER
