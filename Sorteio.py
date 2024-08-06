1fc0c88ec2654905a01ea63eda900a6323c77086
David Júnior <146877886+GitDavidJr@users.noreply.github.com>
2024-08-05 21:26:45 -0300

    first commit

---
+++ Sorteio.py
@@ -0,0 +1,64 @@
+import flet as ft
+
+numero: str
+tamahoCaixa = 200
+
+
+def main(page: ft.Page):
+
+    numeroSorteado = ft.TextField(label="Numero que vai ser sorteado", width=tamahoCaixa)
+
+    sorteio_img = ft.Image(src="sorteio.png", width=300, height=70)
+
+    numeroInicio = ft.TextField(label="Primeiro numero", width=tamahoCaixa)
+    numeroFim = ft.TextField(label="Ultimo numero", width=tamahoCaixa)
+
+    def sortearNum(e):
+        page.add(ft.Text(
+            f"NUMERO SORTEADO: {numero}",
+            size=40,
+            color=ft.colors.BLUE,
+            weight=ft.FontWeight.BOLD,
+            italic=True,
+            shadow=ft.BoxShadow(
+            spread_radius=5,
+            blur_radius=10,
+            color=ft.colors.GREY,
+            offset=ft.Offset(3, 3)
+            )
+            )
+        )
+
+    def start(e):
+
+        global numero
+        numero = numeroSorteado.value
+        page.clean()
+        page.add(
+            ft.Column(
+            [
+                sorteio_img,
+                numeroInicio,
+                numeroFim,
+                ft.ElevatedButton(text="Sortear", on_click=sortearNum)
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+            )   
+        )
+
+    page.vertical_alignment=ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment=ft.CrossAxisAlignment.CENTER
+
+    page.add(
+        ft.Column(
+            [
+                numeroSorteado,
+                ft.ElevatedButton(text="Começar", on_click=start)
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+        )
+    )
+
+ft.app(target=main)
