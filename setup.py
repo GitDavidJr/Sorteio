04f9e2a1de57b6ba4ce2c695c8406da618469901
David Júnior <davidjunior051204@gmail.com>
2024-08-07 01:15:02 -0300

    sorteio 0.2

---
+++ setup.py
@@ -0,0 +1,17 @@
+import sys
+from cx_Freeze import setup, Executable
+
+
+build_exe_options = {"packages": ["os"], "includes": ["flet"]}
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+setup(
+    name="Sorteador de Numeros",
+    version="0.2",
+    description="Sorteador de numeros aleatorios",
+    options={"build_exe": build_exe_options},
+    executables=[Executable("Sorteio.py", base=base)]
+)
